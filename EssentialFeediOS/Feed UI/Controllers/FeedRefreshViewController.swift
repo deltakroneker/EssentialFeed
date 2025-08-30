b346bb4e9772361cb0b4d079d3456c43db7334b8
Nikola Milic <email@nikolamilic.xyz>
2025-08-30 08:59:21 +0200

    Decouple FeedRefreshViewController from the concrete FeedPresenter dependency with a closure.

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -9,15 +9,15 @@ import UIKit
 
 public final class FeedRefreshViewController: NSObject, FeedLoadingView {
     public lazy var view = loadView()
-
-    private let presenter: FeedPresenter
-
-    init(presenter: FeedPresenter) {
-        self.presenter = presenter
+    
+    private let loadFeed: () -> Void
+    
+    init(loadFeed: @escaping () -> Void) {
+        self.loadFeed = loadFeed
     }
-
+    
     @objc func refresh() {
-        presenter.loadFeed()
+        loadFeed()
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
