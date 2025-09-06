8d445185a6969f0c3e9d677852d225091fbeaadd
Nikola Milic <email@nikolamilic.xyz>
2025-09-06 08:18:20 +0200

    Move ErrorView to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeed
+//
+//  Created by Nikola Milic on 6.9.25..
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
