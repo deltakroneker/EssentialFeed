d001c4a8bd3d174587654b2cdd6a63bf788419a0
Nikola Milic <email@nikolamilic.xyz>
2025-11-09 10:38:13 +0100

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -22,9 +22,23 @@ class FeedAcceptanceTests: XCTestCase {
 		XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData())
 	}
 
-	func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
-
-	}
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+        let sharedStore = InMemoryFeedStore.empty
+        let onlineFeed = launch(httpClient: .online(response), store: sharedStore)
+        
+        onlineFeed.simulateAppearance()
+        
+        onlineFeed.simulateFeedImageViewVisible(at: 0)
+        onlineFeed.simulateFeedImageViewVisible(at: 1)
+        
+        let offlineFeed = launch(httpClient: .offline, store: sharedStore)
+        
+        offlineFeed.simulateAppearance()
+        
+        XCTAssertEqual(offlineFeed.numberOfRenderedFeedImageViews(), 2)
+        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 0), makeImageData())
+        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 1), makeImageData())
+    }
 
 	func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
 
