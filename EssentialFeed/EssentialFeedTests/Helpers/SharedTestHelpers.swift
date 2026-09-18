5e2f7d61c50064b2a87344f6a181e56e6b0217e2
Nikola Milic <email@nikolamilic.xyz>
2026-09-18 16:54:44 +0200

    Inject calendar to make tests reliable

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -12,7 +12,7 @@ func anyNSError() -> NSError {
 }
 
 func anyURL() -> URL {
-    return  URL(string: "https://any-url.com")!
+    return URL(string: "http://any-url.com")!
 }
 
 func anyData() -> Data {
@@ -35,11 +35,11 @@ extension Date {
         return self + seconds
     }
     
-    func adding(minutes: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    func adding(minutes: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        return calendar.date(byAdding: .minute, value: minutes, to: self)!
     }
 
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    func adding(days: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        return calendar.date(byAdding: .day, value: days, to: self)!
     }
 }
